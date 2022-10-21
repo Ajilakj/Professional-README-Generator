484d4e20c131fd0f21d43740b383db99aef56439
AKJ <kjajila@gmail.com>
2022-10-20 21:58:37 -0400

    add licence link and desc

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,22 +14,69 @@ function renderLicenseBadge(license) {
   else if(license==='NPM'){
     badge="https://shields.io/badge/license-MIT-green";
   }
-  else if(license==='GitLab'){
+  else if(license==='Reuse'){
     badge="https://shields.io/badge/reuse-compliant-green";
+  }
+  else{
+    badge='';
   }
   return badge;
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link;
+  if(license==='APM'){
+    link="/apm/l/:packageName";
+  }
+  else if(license==='Bower'){
+    link="/bower/l/:packageName";
+  }
+  else if(license==='GitHub'){
+    link="/github/license/:user/:repo";
+  }
+  else if(license==='NPM'){
+    link="/npm/l/:packageName";
+  }
+  else if(license==='Reuse'){
+    link="/reuse/compliance/:remote+";
+  }
+  else{
+    link='';
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let desc;
+  if(license==='APM'){
+    desc="apm";
+  }
+  else if(license==='Bower'){
+    desc="bower";
+  }
+  else if(license==='GitHub'){
+    desc="github";
+  }
+  else if(license==='NPM'){
+    desc="npm";
+  }
+  else if(license==='Reuse'){
+    desc="reuse";
+  }
+  else{
+    desc='';
+  }
+  return desc;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   let licenseBadge=renderLicenseBadge(data.license);
+  let licenseLink=renderLicenseBadge(data.license);
+  let licenseDesc=renderLicenseBadge(data.license);
   return `# ${data.title}\n\n
   ## Table of Contents
   * [Description](#description)
@@ -40,7 +87,7 @@ function generateMarkdown(data) {
   * [GitHub](#github)
   * [Questions](#questions)
   ## Description\n${data.description}\n\n
-  ## License\n${data.license} ![](${licenseBadge})\n\n
+  ## License\n${data.license} ![](${licenseBadge})\n[License link]( ${licenseLink})\n${licenseDesc}\n\n
   ## Installation\n${data.installation}\n\n
   ## Usage\n${data.usage}\n\n
   ## Contribution\n${data.contribution}\n\n
